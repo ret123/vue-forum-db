b8f28b4920f3340356b54ef6cd724a8a8a181f5f
ret123 <rathish83@gmail.com>
2019-12-26 11:56:56 +0530

    Initial commit

---
+++ migrations/20191226112945_user.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+      table.increments();
+      table.text('display_name').notNullable();
+      table.text('email').notNullable();
+      table.text('google_id').notNullable();
+      table.boolean('banned').notNullable().notNullable().default(false);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
