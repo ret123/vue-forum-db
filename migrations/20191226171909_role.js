fd018a33196053b1225f2a41ebc1a81cf143e400
ret123 <rathish83@gmail.com>
2019-12-27 11:39:10 +0530

    Add role and user table

---
+++ migrations/20191226171909_role.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('role', (table) => {
+        table.increments();
+        table.text('name');
+      
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('role');
+  };
+  
---
+++ seeds/01-roles.js
@@ -0,0 +1,14 @@
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('role').del();
+  await knex.raw('ALTER SEQUENCE role_id_seq restart with 3');
+    
+  // Inserts seed entries
+  return knex('role').insert([
+    {id: 1, name: 'user'},
+    {id: 2, name: 'moderator'},
+    {id: 3, name: 'administrator'}
+  ]);
+   
+};
